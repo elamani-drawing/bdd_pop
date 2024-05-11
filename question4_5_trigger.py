912f41cef679ee9006cd25be29aa7db3d08aaab5
Au = Or[79]² <assanisaidelamani@gmail.com>
2024-05-11 22:06:27 +0200

    rendu final

--- question4_5_trigger.py
+++ question4_5_trigger.py
@@ -10,12 +10,16 @@ def restore_data(connection, params):
     insert_stats_population_fragment(connection, params, annee="2020", init_metadonnee= True, verbose=True)
     insert_dep_file_fragment(connection, params,"2021", verbose=True)
     execute_view_bdd(connection, params) # genere les view du fichier view.sql
-    execute_alter_procedure(connection, params) # execute le fichier alter.sql et procedure.sql, comme indiquer a la question 3
+    # execute le fichier alter.sql et procedure.sql, comme indiquer a la question 3
+    # execute le fichier trigger.sql pour empecher insert/update/ delete de departement et region
+    # calcul et met a jour les calculs de population à l'ajout d'une nouvelle année de recensement quand le resultat est pertinent
+    execute_alter_procedure(connection, params) 
     execute_sql_file(connection, params["sql_requete"]["trigger_sql"]) 
     
         
 # Vous pouvez commenter cette ligne apres qu'elle est etait executé au moin une fois 
 restore_data(connection, params) 
+print("- Une erreur est attendu:")
 # L'insertion est censé echoué à cause des triggers qui empeche l'insertion sur Région et Département
 insert_region(connection, 999, "Région qui n'existe pas.") 
 
--- question6_explain.py
+++ question6_explain.py
@@ -9,9 +9,14 @@ def restore_data(connection, params):
     initialise_db(connection, params, flag_reset_bdd=True)
     insert_stats_population_fragment(connection, params, annee="2020", init_metadonnee= True, verbose=True)
     insert_dep_file_fragment(connection, params,"2021", verbose=True)
+    execute_view_bdd(connection, params) # genere les view du fichier view.sql 
     execute_view_bdd(connection, params) # genere les view du fichier view.sql
-    execute_alter_procedure(connection, params) # execute le fichier alter.sql et procedure.sql, comme indiquer a la question 3
+    # execute le fichier alter.sql et procedure.sql, comme indiquer a la question 3
+    # execute le fichier trigger.sql pour empecher insert/update/ delete de departement et region
+    # calcul et met a jour les calculs de population à l'ajout d'une nouvelle année de recensement quand le resultat est pertinent
+    execute_alter_procedure(connection, params)
     execute_sql_file(connection, params["sql_requete"]["trigger_sql"]) 
+    
     
         
 # Vous pouvez commenter cette ligne apres qu'elle est etait executé au moin une fois 
